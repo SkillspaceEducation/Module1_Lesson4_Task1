3e52eb8be4bc054bd0afdea351844ab0e922a30b
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-19 09:45:31 +0300

    Создайте переменную для массива из 10 элементов. Заполните его произвольными значениями целочисленного типа. Выведите на экран элементы, стоящие на четных позициях.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+/*
+Создайте переменную для массива из 10 элементов.
+Заполните его произвольными значениями целочисленного типа.
+Выведите на экран элементы, стоящие на четных позициях.
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int arrayInitLen = 10; // Размер исходного массива
+        int[] arrayInit = new int[arrayInitLen]; // Исходный массив
+        int[] arrayEvenInd = new int[arrayInitLen / 2]; // Массив для вывода элементов исходного массива с четными
+        // индексами, его размер в два раза меньше исходного
+        Random rand = new Random();
+        for (int i = 0; i < arrayInit.length; i++) {
+            arrayInit[i] = rand.nextInt(50); // Заполняем массив случайными числами не более 50
+        }
+        System.out.println("Исходный массив\n" + Arrays.toString(arrayInit));
+
+        for (int i = 0; i < arrayInit.length; i++) {
+            if (i % 2 == 0) {                        // Если элемент исходного массива имеет четный индекс,
+                arrayEvenInd[i / 2] = arrayInit[i];   // добавляем этот элемент в дополнительный массив
+            }
+        }
+        System.out.println("Массив с элементами исходного массива, имеющими четный индекс\n" +
+                Arrays.toString(arrayEvenInd));
     }
 }
